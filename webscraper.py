5344be1137ab020927b37eb5376e49504910d608
james <james.willliamson.2018@uni.strath.ac.uk>
2021-07-22 15:06:19 +0100

    Currently scarping specifically the course content part of the web page

--- webscraper.py
+++ webscraper.py
@@ -2,20 +2,33 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import webbrowser
 
-testurl = 'https://www.strath.ac.uk/courses/undergraduate/'
+defaultURL = 'https://www.strath.ac.uk'
 
-testpage = urlopen(testurl)
+coursesSearchURL = '/courses/undergraduate/'
 
-testhtml = testpage.read().decode("utf-8")
+defaultPage = urlopen(defaultURL + coursesSearchURL)
 
-testsoup = BeautifulSoup(testhtml, 'lxml')
+defaultHtml = defaultPage.read().decode("utf-8")
 
-courses = testsoup.find_all('a', class_="course-search-result__link")
+soup = BeautifulSoup(defaultHtml, 'lxml')
+
+courses = soup.find_all('a', class_="course-search-result__link")
 x = 0
 for course in courses:
 
-    print(course['href'])
-    webbrowser.open('https://www.strath.ac.uk' + course['href'])
+    #print(course['href'])
+
+    courseURL = (defaultURL + course['href'])
+  #  webbrowser.open(courseURL)
+    coursePage = urlopen(courseURL)
+    courseHtml = coursePage.read().decode("utf-8")
+    courseContent = BeautifulSoup(courseHtml, 'lxml')
+    test = courseContent.find('div', {"id": "coursecontent"})
+    containerDiv = test('div', {'class': "container"})
+    print(containerDiv)
+
+
+
     x += 1
     if x == 1:
         break
@@ -23,4 +36,6 @@ for course in courses:
 
 
 
+# <ul class="course-module-nav-tabs nav nav-tabs off-canvas-tabs" id="myTab" role="tablist">
+#                             <li class="course-module-header nav-item" aria-controls="tab-0-panel"><a class="nav-link grey-block-button off-canvas-button-open active" id="" data-toggle="tab" href="#tab-0-panel" role="tab" aria-controls="home" aria-selected="true">Year 1</a></li><li class="course-module-header nav-item" aria-controls="tab-1-panel"><a class="nav-link grey-block-button off-canvas-button-open" id="" data-toggle="tab" href="#tab-1-panel" role="tab" aria-controls="home" aria-selected="true">Year 2</a></li><li class="course-module-header nav-item" aria-controls="tab-2-panel"><a class="nav-link grey-block-button off-canvas-button-open" id="" data-toggle="tab" href="#tab-2-panel" role="tab" aria-controls="home" aria-selected="true">Year 3</a></li><li class="course-module-header nav-item" aria-controls="tab-3-panel"><a class="nav-link grey-block-button off-canvas-button-open" id="" data-toggle="tab" href="#tab-3-panel" role="tab" aria-controls="home" aria-selected="true">Year 4</a></li></ul>
 
