72ad9b0b2543abf1e8af2da94dbeb115fb0aa0b2
james <james.willliamson.2018@uni.strath.ac.uk>
2021-07-22 16:36:13 +0100

    Made the code a little more readble

--- webscraper.py
+++ webscraper.py
@@ -2,40 +2,49 @@ from bs4 import BeautifulSoup
 from urllib.request import urlopen
 import webbrowser
 
+# Loading the strathclyde website
 defaultURL = 'https://www.strath.ac.uk'
 
-coursesSearchURL = '/courses/undergraduate/'
 
-defaultPage = urlopen(defaultURL + coursesSearchURL)
+# This extension takes you to all the university's courses
+courseSearchExtension = '/courses/undergraduate/'
+
+# The next two lines are basically opening the html file
+defaultPage = urlopen(defaultURL + courseSearchExtension)
 
 defaultHtml = defaultPage.read().decode("utf-8")
 
+# I think this parses the html, might've forgotten
 soup = BeautifulSoup(defaultHtml, 'lxml')
 
+# Basically finds all the classes individual extensions and stores them in a variable
 courses = soup.find_all('a', class_="course-search-result__link")
+
 x = 0
+# Loops through all courses
 for course in courses:
 
-    #print(course['href'])
+# Outputs a course's
+    print(course['href'])
 
     courseURL = (defaultURL + course['href'])
+
+# Will open course web page in a browser
   #  webbrowser.open(courseURL)
     coursePage = urlopen(courseURL)
     courseHtml = coursePage.read().decode("utf-8")
     courseContent = BeautifulSoup(courseHtml, 'lxml')
+
+## This is used to try and find a specific div
     test = courseContent.find('div', {"id": "coursecontent"})
     containerDiv = test('div', {'class': "container"})
-    print(containerDiv)
+    #print(containerDiv)
 
 
 
-    x += 1
-    if x == 1:
-        break
+    # Makes the code only run once
+    break
 
 
 
-
-# <ul class="course-module-nav-tabs nav nav-tabs off-canvas-tabs" id="myTab" role="tablist">
-#                             <li class="course-module-header nav-item" aria-controls="tab-0-panel"><a class="nav-link grey-block-button off-canvas-button-open active" id="" data-toggle="tab" href="#tab-0-panel" role="tab" aria-controls="home" aria-selected="true">Year 1</a></li><li class="course-module-header nav-item" aria-controls="tab-1-panel"><a class="nav-link grey-block-button off-canvas-button-open" id="" data-toggle="tab" href="#tab-1-panel" role="tab" aria-controls="home" aria-selected="true">Year 2</a></li><li class="course-module-header nav-item" aria-controls="tab-2-panel"><a class="nav-link grey-block-button off-canvas-button-open" id="" data-toggle="tab" href="#tab-2-panel" role="tab" aria-controls="home" aria-selected="true">Year 3</a></li><li class="course-module-header nav-item" aria-controls="tab-3-panel"><a class="nav-link grey-block-button off-canvas-button-open" id="" data-toggle="tab" href="#tab-3-panel" role="tab" aria-controls="home" aria-selected="true">Year 4</a></li></ul>
 
