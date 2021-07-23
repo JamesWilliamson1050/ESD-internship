5351e4b7a3c456f90a59c3f116efecdbc6accc91
james <james.willliamson.2018@uni.strath.ac.uk>
2021-07-23 13:33:01 +0100

    Writing module information to a file now. Need to find a way to not repeat modules

--- webscraper.py
+++ webscraper.py
@@ -1,10 +1,8 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-
 # Loading the strathclyde website
 defaultURL = 'https://www.strath.ac.uk'
-
 
 # This extension takes you to all the university's courses
 courseSearchExtension = '/courses/undergraduate/'
@@ -20,27 +18,20 @@ soup = BeautifulSoup(defaultHtml, 'lxml')
 # Basically finds all the classes individual extensions and stores them in a variable
 courses = soup.find_all('a', class_="course-search-result__link")
 
-
+moduleInfoList = []
 # Loops through all courses
 for course in courses:
 
-# Outputs a course's
-
-
-
+    # Outputs a course's
     courseURL = (defaultURL + course['href'])
 
-
-
-
-
-# Will open course web page in a browser
+    # Will open course web page in a browser
 
     coursePage = urlopen(courseURL)
     courseHtml = coursePage.read().decode("utf-8")
     courseContent = BeautifulSoup(courseHtml, 'lxml')
 
-## This is used to try and find a specific div
+    ## This is used to try and find a specific div
     modules = courseContent.find_all('div', {'class': "course-module"})
 
     for module in modules:
@@ -49,9 +40,20 @@ for course in courses:
 
         moduleTitleText = moduleTitle.text
         moduleDescriptionText = moduleDescription.text
-        print(moduleTitleText)
-        print(moduleDescriptionText)
-        print()
+        # print(moduleTitleText)
+        # print(moduleDescriptionText)
+        # print()
+
+        f = open('test.txt', 'a', encoding='utf-8')
+        f.write(moduleTitleText)
+        f.write(moduleDescriptionText)
+        f.close()
+
+
+
+
+
+
 
 
 
@@ -59,7 +61,3 @@ for course in courses:
 
     # Makes the code only run once
     break
-
-
-
-
