c54783f493577d11c0b019bcd8341faa6d6e6afa
james <james.willliamson.2018@uni.strath.ac.uk>
2021-07-23 11:48:35 +0100

    Now removing all tags and keeping only the module titles and descriptions

--- webscraper.py
+++ webscraper.py
@@ -1,12 +1,9 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-from requests_html import HTMLSession
-import webbrowser
+
 
 # Loading the strathclyde website
 defaultURL = 'https://www.strath.ac.uk'
-
-session = HTMLSession()
 
 
 # This extension takes you to all the university's courses
@@ -23,7 +20,7 @@ soup = BeautifulSoup(defaultHtml, 'lxml')
 # Basically finds all the classes individual extensions and stores them in a variable
 courses = soup.find_all('a', class_="course-search-result__link")
 
-x = 0
+
 # Loops through all courses
 for course in courses:
 
@@ -33,28 +30,32 @@ for course in courses:
 
     courseURL = (defaultURL + course['href'])
 
-    renderSession = session.get(courseURL)
 
-    # The first time this runs it will download chromium, the opensource version of google chrome
-    # renderSession.html.render(sleep=1,  keep_page=True, scrolldown=1)
-    #
-    # modules = renderSession.html.find('#moduel-content')
 
-    # for module in modules:
-    #     print(module)
+
 
 # Will open course web page in a browser
-    #webbrowser.open(courseURL)
+
     coursePage = urlopen(courseURL)
     courseHtml = coursePage.read().decode("utf-8")
     courseContent = BeautifulSoup(courseHtml, 'lxml')
 
 ## This is used to try and find a specific div
-    test = courseContent.find_all('div', {'class': "course-module"})
-    #containerDiv = test('div', {'class': "tab-inner"})
-    print(test)
+    modules = courseContent.find_all('div', {'class': "course-module"})
+
+    for module in modules:
+        moduleTitle = module.find('h5')
+        moduleDescription = module.find('div', {'class': "course-module-content-inner"})
+
+        moduleTitleText = moduleTitle.text
+        moduleDescriptionText = moduleDescription.text
+        print(moduleTitleText)
+        print(moduleDescriptionText)
+        print()
 
 
+
+    # print(modules)
 
     # Makes the code only run once
     break
