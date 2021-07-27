72f60f5abd630d4721ea8022f2bd4a187034c1b7
james <james.willliamson.2018@uni.strath.ac.uk>
2021-07-27 15:02:23 +0100

    Working on adding additional information about courses and modules also looking at automating excel spreadsheets

--- webscraper.py
+++ webscraper.py
@@ -1,7 +1,6 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-
 # Loading the strathclyde website
 defaultURL = 'https://www.strath.ac.uk'
 
@@ -26,27 +25,27 @@ for course in courses:
     # Outputs a course's
     courseURL = (defaultURL + course['href'])
 
-    # Will open course web page in a browser
-
     coursePage = urlopen(courseURL)
     courseHtml = coursePage.read().decode("utf-8")
     courseContent = BeautifulSoup(courseHtml, 'lxml')
 
     ## This is used to try and find a specific div
     modules = courseContent.find_all('div', {'class': "course-module"})
-    x = 0
+
+    # print(modules)
+
     for module in modules:
         moduleTitle = module.find('h5')
         moduleDescription = module.find('div', {'class': "course-module-content-inner"})
-        moduleCode = module.find('div', {'class': "course-module-content"})
-
+        # moduleCode = module.find('div', {'class': "course-module-content"})
+        # print(moduleCode)
 
         moduleTitleText = moduleTitle.text
         moduleDescriptionText = moduleDescription.text
+        # moduleYearText = moduleYear.text
+        print(moduleTitleText)
         moduleTitleText = moduleTitleText.strip()
         moduleTitleText = moduleTitleText.rstrip()
-
-
 
         moduleDescList = []
         moduleDescList.append(moduleDescriptionText)
@@ -56,17 +55,11 @@ for course in courses:
         else:
             moduleInfoDict[moduleTitleText].extend(moduleDescriptionText)
 
-
-
-        #Opens, reads and writes data to test.txt
+        # Opens, reads and writes data to test.txt
         # readf = open('test.txt', 'r+', encoding='utf-8')
         # fcontent = readf.read()
 
-
         # Only adding modules that aren't already in the text file
-
-
-
 
     #     if moduleTitleText not in fcontent:
     #         readf.write(moduleTitleText)
@@ -74,11 +67,21 @@ for course in courses:
     #         readf.write('\n')
     #
     # readf.close()
+    break
+
+# doesn't work properly
+# def write_dic_to_file():
+#     try:
+#         file = open('test.txt', 'a')
+#         file.write(json.dumps(moduleInfoDict))
+#         file.close
+#     except:
+#         print("Unable to write to file")
 
 
+if __name__ == '__main__':
+    print()
+    # write_dic_to_file()
+    # print(moduleInfoDict.keys())
 
-    #Adding
-
-
-
-
+    # Adding
