fbc9135a1f15dfec7ed7a89a88130699824af947
james <james.willliamson.2018@uni.strath.ac.uk>
2021-08-16 09:02:02 +0100

    Committing to switch branches

--- fillWebForm.py
+++ fillWebForm.py
@@ -13,24 +13,29 @@ PATH = "C:\Program Files (x86)\chromedriver.exe"
 def fillForm(Code):
     driver = webdriver.Chrome(PATH)
     driver.get("https://but.mis.strath.ac.uk/classcatalogue/control/searchpage")
+
     wait = WebDriverWait(driver, 10)
 
     # Used to find and click on accept cookies button
     cookies = wait.until(ec.visibility_of_element_located((By.ID, 'ccc-notify-accept')))
     cookies.click()
 
+
     # Find and click on search link
     searchLink = driver.find_element_by_link_text('Search')
     searchLink.click()
+
 
     # Filling and submit class catalogue form with a code
     classCode = driver.find_element_by_name('code')
     classCode.send_keys(Code)
     classCode.submit()
 
+
     # Click on the link to get the classes information
     classTitleLink = driver.find_element_by_xpath('//*[@id="commonContent"]/table[4]/tbody/tr[2]/td[2]/a')
     classTitleLink.click()
+
 
     # Finding a printing the department of a module
     department = driver.find_element_by_xpath(
