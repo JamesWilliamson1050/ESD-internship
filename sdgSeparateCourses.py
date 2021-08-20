80e430da5345894be4481b59fb340fd6a60ac63d
james <james.willliamson.2018@uni.strath.ac.uk>
2021-08-20 11:25:39 +0100

    Now writing the separated courses to a CSV with the keyword comparison

---
+++ sdgSeparateCourses.py
@@ -0,0 +1,107 @@
+import csv
+
+def readCSV():
+    course = []
+    # try:
+    with open('SeparateCourses.csv', 'r', encoding="utf-8") as csv_file:
+        reader = csv.reader(csv_file)
+        for r in reader:
+            #print(len(r[1]))
+            if r != [' ']:
+                course.append(r)
+            elif r == [' ']:
+                separateModuleInfo(course)
+                course.clear()
+                # Pass info to another function
+
+        csv_file.close()
+    # except Exception:
+    #     print("Could not find CSV file")
+
+
+def separateModuleInfo(course):
+    if len(course) >=1:
+        courseTitle = course[0]
+        modules = course[1:]
+        with open('sdgSeparateCourses.csv', 'a', encoding="utf-8", newline='') as sdgSeparate:
+            writer = csv.writer(sdgSeparate)
+            writer.writerow(courseTitle)
+        for module in modules:
+            moduleTitle = module[1]
+            moduleDesc = module[2]
+            degreeLevel = module[3]
+            output = filterKeywords(moduleTitle, moduleDesc)
+
+            for key in output.keys():
+                with open('sdgSeparateCourses.csv', 'a', encoding="utf-8", newline='') as sdgSeparate:
+                    writer = csv.writer(sdgSeparate)
+                    writer.writerow([key] + [degreeLevel] + output[key])
+
+
+
+
+
+
+
+
+
+
+def filterKeywords(moduleTitle, moduleDescription):
+    sdg = {}
+    output = {}
+    moduleDescription = moduleDescription.lower().strip()
+    with open("KEYWORDS.csv") as keywords_file:
+
+        for inc, line in enumerate(keywords_file.readlines()):
+            sdgNow = [word for word in line.lower().strip().split(',') if word not in ['', '\n', ' ']]
+            sdg[inc + 1] = sdgNow
+
+    keywords_file.close()
+
+    sdgList = []
+
+    for index in sdg.keys():
+
+        if any(word in moduleDescription for word in sdg[index]):
+
+            sdgList.append("YES")
+        else:
+            sdgList.append("NO")
+    output[moduleTitle] = sdgList
+
+    return output
+
+def writeOutput(moduleTitle, moduleDesc, degreeLevel):
+
+    with open('sdgSeparateCourses.csv', 'a', encoding="utf-8", newline='') as webOutputs:
+        header = ['Module Title']
+        toWrite = ''
+
+        for sdgNo in range(1, 18):
+            sdgHeader = 'SDG' + str(sdgNo)
+            header.append(sdgHeader)
+
+        writer = csv.writer(webOutputs)
+        writer.writerow(header)
+        output = filterKeywords(moduleTitle, moduleDesc)
+
+
+
+        for key in output.keys():
+            toWrite = toWrite + (key.replace(',', ';') + ',' + degreeLevel + ','.join(output[key]) +  '\n')
+        webOutputs.write(toWrite)
+
+def writeHeader():
+    with open('sdgSeparateCourses.csv', 'w', encoding="utf-8", newline='') as sdgSeparate:
+        header = ['Module Title', 'Degree Level']
+
+        for sdgNo in range(1, 18):
+            sdgHeader = 'SDG' + str(sdgNo)
+            header.append(sdgHeader)
+
+        writer = csv.writer(sdgSeparate)
+        writer.writerow(header)
+
+if __name__ == '__main__':
+    writeHeader()
+    readCSV()
