352edf19f5547edac9cd521d8d9852a6d15df2cb
james <james.willliamson.2018@uni.strath.ac.uk>
2021-08-20 09:04:56 +0100

    Commiting my find deparatment code, so I can checkout another branch

--- findDepartmentFaculty.py
+++ findDepartmentFaculty.py
@@ -1,7 +1,8 @@
 import pandas as pd
+import WebFormFiller as wf
+
 
 def readCSV():
-
     try:
         df = pd.read_csv('moduleInfoClassCodes.csv', sep=',', encoding="utf-8")
         return df
@@ -12,7 +13,27 @@ def readCSV():
 
 def searchForDepAndFac():
     df = readCSV()
-    print(df)
+    moudleCodes = df['Module Code']
+    df['Department'] = ''
+    df['Faculty'] = ''
+    print(df.columns)
+
+    # TODO finish adding the faculty and department of a module
+    # TODO Make sure that the department and faculty are being added as headers
+    # Potential solution
+    # Iterate over the df
+    # For ever code that is a string pass it to the form filler function from webFormFiller module
+    # Extract the department and faculty
+    # Add the to data frame (Probably use the in build pandas function at and use the current index to do this)
+    # Then you need to write the dataframe to a CSV file
+
+    for mc in moudleCodes:
+        if type(mc) == str:
+            department, faculty = wf.fillForm(mc)
+            df['Department'] = department
+            break
+
+
 
 
 if __name__ == '__main__':
